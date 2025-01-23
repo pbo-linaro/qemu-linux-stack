31514b48689fa932b8f1ad26e0dd4ef8e7ae3148
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-01-22 17:13:20 -0800

    container: mount home as well

--- container.sh
+++ container.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 podman build -t build-smmuv3-stack - < ./Dockerfile
 
 podman run \
-    -it --rm -v $(pwd):$(pwd) -w $(pwd) --init \
+    -it --rm -v $(pwd):$(pwd) -w $(pwd) -v $HOME:$HOME --init \
     --network host \
     -e DISABLE_CONTAINER_CHECK=1 \
     build-smmuv3-stack \
