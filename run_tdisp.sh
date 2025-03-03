d9d271a7875a2bc0483a5ca785bc0a8172f2ad57
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-03-03 13:31:32 -0800

    run_tdisp.sh

---
+++ run_tdisp.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# -lt 2 ]; then
+    echo "usage: qemu_src_dir qemu_aarch64_cmd"
+    exit 1
+fi
+
+qemu_src_dir=$1; shift
+
+tdisp_data="$qemu_src_dir/tests/data/tdisp"
+
+if [ ! -d $tdisp_data ]; then
+    echo "can't find tdisp data at $tdisp_data"
+    exit 1
+fi
+
+./run.sh "$@" \
+-device pcie-root-port,id=pcie.1 \
+-device tdisp-testdev,bus=pcie.1,spdm-responder=spdm.0 \
+-object spdm-responder-libspdm,id=spdm.0,\
+certs=$tdisp_data/rsa3072/device.certchain.der,\
+keys=$tdisp_data/rsa3072/device.key,\
+certs=$tdisp_data/ecp256/device.certchain.der,\
+keys=$tdisp_data/ecp256/device.key,\
+certs=$tdisp_data/ecp384/device.certchain.der,\
+keys=$tdisp_data/ecp384/device.key
