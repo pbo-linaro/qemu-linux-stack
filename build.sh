45da0f64bf73b747bba75dfd746a2ca20909ff5d
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-05-29 11:08:34 -0700

    common init files for rootfs

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ build_rootfs()
 {
     r=$1
     rm -f out/$r.ext4
-    podman build -t build-linux-stack-$r rootfs/$r
+    podman build -t build-linux-stack-$r \
+        --build-context common=rootfs/common rootfs/$r
     container=$(podman create build-linux-stack-$r)
     podman export -o /dev/stdout $container > out/$r.tar
     ./container.sh /sbin/mke2fs -t ext4 -d out/$r.tar out/$r.ext4 10g
