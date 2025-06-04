bd134ff4a72e562f645914cab0cde8cd29ff7d5a
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-06-04 15:35:29 -0700

    archive_artifacts script

--- README.md
+++ README.md
@@ -17,4 +17,8 @@ You need to have podman and qemu-user-binfmt installed.
 
 # to debug qemu itself:
 ./run.sh gdb --args /path/to/qemu-system-aarch64
+
+# to create an archive containing the whole stack:
+./build.sh
+./archive_artifacts.sh stack.tar.gz
 ```
---
+++ archive_artifacts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+    echo "usage: out_tar_gz"
+    exit 1
+fi
+
+out=$1; shift
+
+# create a sparse archive with:
+# - kernel
+# - guest rootfs
+# - host rootfs
+tar czvfS $out out/Image out/host.ext4 out/guest.ext4
