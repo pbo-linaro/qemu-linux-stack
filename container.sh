f7374a4129107ff02f032248bf3bb87df2867cac
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-06-10 15:17:31 -0700

    build: build linux with ccache

--- container.sh
+++ container.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 podman build -t build-linux-stack - < ./Dockerfile
 
 podman run \
-    -it --rm -v $(pwd):$(pwd) -w $(pwd) -v $HOME:$HOME --init \
+    -it --rm -v $(pwd):$(pwd) -w $(pwd) -v $HOME:$HOME -e HOME=$HOME --init \
     --network host \
     --privileged \
     -e DISABLE_CONTAINER_CHECK=1 \
