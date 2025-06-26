040347a75d6dc892b9f573774d84157d4c5c7cd5
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-06-26 15:46:20 -0700

    extract build_rootfs

--- build.sh
+++ build.sh
@@ -2,26 +2,8 @@
 
 set -euo pipefail
 
-mkdir -p out
-
-build_rootfs()
-{
-    r=$1
-    rm -f out/$r.ext4
-    podman build -t build-linux-stack-$r \
-        --build-context common=rootfs/common rootfs/$r
-    container=$(podman create build-linux-stack-$r)
-    podman export -o /dev/stdout $container > out/$r.tar
-    ./container.sh /sbin/mke2fs -t ext4 -d out/$r.tar out/$r.ext4 10g
-    podman rm -f $container
-}
-
 ./container.sh ./build_kernel.sh
 ./container.sh ./build_uboot.sh
-if [ -d linux/tools/dmate ]; then
-    ./container.sh ./build_dmatest.sh
-fi
-build_rootfs host
-build_rootfs guest
+./build_rootfs.sh
 
 du -hc out/*
---
+++ build_rootfs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+mkdir -p out
+
+build_rootfs()
+{
+    r=$1
+    rm -f out/$r.ext4
+    podman build -t build-linux-stack-$r \
+        --build-context common=rootfs/common rootfs/$r
+    container=$(podman create build-linux-stack-$r)
+    podman export -o /dev/stdout $container > out/$r.tar
+    ./container.sh /sbin/mke2fs -t ext4 -d out/$r.tar out/$r.ext4 10g
+    podman rm -f $container
+}
+
+build_rootfs host
+build_rootfs guest
