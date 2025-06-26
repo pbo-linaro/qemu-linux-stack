1dda71c8e61d2e25ff5b0f20e99830d16cff4d93
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-06-26 15:58:26 -0700

    script to build arm trusted firmware

---
+++ build_arm_trusted_firmware.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ -z "${DISABLE_CONTAINER_CHECK:-}" ]; then
+    echo "run command using ./container.sh ./build_arm_trusted_firmware.sh"
+    exit 1
+fi
+
+clone_tfa()
+{
+    if [ ! -d arm-trusted-firmware ]; then
+        git clone \
+            https://github.com/ARM-software/arm-trusted-firmware \
+            --single-branch --branch master \
+            arm-trusted-firmware
+    fi
+}
+
+build_tfa()
+{
+    pushd arm-trusted-firmware
+    make PLAT=qemu BL33=../out/u-boot.bin all fip -j$(nproc)
+    dd if=build/qemu/release/bl1.bin of=flash.bin bs=4096 conv=notrunc
+    dd if=build/qemu/release/fip.bin of=flash.bin seek=64 bs=4096 conv=notrunc
+    popd
+}
+
+output()
+{
+    mkdir -p out
+    rsync ./arm-trusted-firmware/flash.bin out/flash.bin
+}
+
+clone_tfa
+build_tfa
+output
