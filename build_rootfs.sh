9f2dd373cc95a25fb824ce3fae5c74df12484627
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-06-27 12:36:51 -0700

    reduce rootfs size

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -14,7 +14,10 @@ build_rootfs()
     # export a tar, and pass it to mke2fs directly
     podman export -o /dev/stdout $container |
         env CONTAINER_NO_TTY= \
-        ./container.sh /sbin/mke2fs -t ext4 -d - out/$r.ext4 10g
+        ./container.sh /sbin/mke2fs -t ext4 -d - out/$r.ext4.tmp 10g
+    # re-sparse it, to get smaller file (mke2fs does not produce minimal files)
+    ./container.sh cp --sparse=always out/$r.ext4.tmp out/$r.ext4
+    rm out/$r.ext4.tmp
     podman rm -f $container
 }
 
