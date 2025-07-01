879ee6a2afdb54bd4893ea3b61eb63a62969464e
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-01 14:08:38 -0700

    RME (Realm Management Extension) stack

---
+++ build_tf_rmm.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+if [ -z "${DISABLE_CONTAINER_CHECK:-}" ]; then
+    ./container.sh ./build_tf_rmm.sh
+    exit 0
+fi
+
+clone()
+{
+    rm -f tf-rmm
+    url=https://github.com/TF-RMM/tf-rmm
+    version=tf-rmm-v0.7.0
+    src=$version-support-lower-pmu-versions
+    if [ ! -d $src ]; then
+        git clone $url --single-branch --branch $version --depth 1 $src
+        pushd $src
+        git submodule update --init --depth 1
+        git am ../patches/rmm-support-lower-pmu-versions.patch
+        popd
+    fi
+    ln -s $src tf-rmm
+}
+
+build()
+{
+    pushd tf-rmm
+    env CROSS_COMPILE=aarch64-linux-gnu- \
+      cmake -DRMM_CONFIG=qemu_virt_defcfg \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DLOG_LEVEL=40 \
+      -S . -B build
+    intercept-build --append \
+    make -C build -j "$(nproc)"
+    sed -i compile_commands.json -e 's/"cc"/"aarch64-linux-gnu-gcc"/'
+    popd
+}
+
+clone
+build
