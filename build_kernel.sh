7382ef1ce8f422c7e906ce074c2b2737620a5b09
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-01 14:08:38 -0700

    RME (Realm Management Extension) stack

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,9 +11,9 @@ fi
 clone()
 {
     rm -f linux
-    url=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
-    version=v6.15
-    src=linux_$version
+    url=https://gitlab.arm.com/linux-arm/linux-cca
+    version=cca-host/v9
+    src=linux_$(echo $version | tr '/' '_')
     if [ ! -d $src ]; then
         git clone $url --single-branch --branch $version --depth 1 $src
     fi
@@ -32,6 +32,9 @@ build()
     scripts/config --enable IOMMUFD
     scripts/config --enable VFIO_DEVICE_CDEV
     scripts/config --enable ARM_SMMU_V3_IOMMUFD
+    # # Enable the configfs-tsm driver that provides the attestation interface
+    scripts/config --enable VIRT_DRIVERS
+    scripts/config --enable ARM_CCA_GUEST
 
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- olddefconfig -j$(nproc)
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- all -j$(nproc)
@@ -41,7 +44,8 @@ build()
 output()
 {
     mkdir -p out
-    rsync ./linux/arch/arm64/boot/Image.gz out/
+    # kvmtool is not able to boot a compressed kernel
+    rsync ./linux/arch/arm64/boot/Image out/
 }
 
 clone
