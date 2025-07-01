2e1214bad5aa378c57df15635843796e5c7974ac
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-01 14:08:38 -0700

    RME (Realm Management Extension) stack

---
+++ build_edk2.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+if [ -z "${DISABLE_CONTAINER_CHECK:-}" ]; then
+    ./container.sh ./build_edk2.sh
+    exit 0
+fi
+
+clone()
+{
+    rm -f edk2
+    url=https://github.com/tianocore/edk2.git
+    version=edk2-stable202505
+    src=$version
+    if [ ! -d $src ]; then
+        git clone $url --single-branch --branch $version --depth 1 $src
+        pushd $src
+        git submodule update --init --depth 1
+        popd
+    fi
+    ln -s $src edk2
+}
+
+build()
+{
+    pushd edk2
+    make -C BaseTools -j $(nproc)
+    export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
+    # always build in debug to enable traces
+    bash -c ". edksetup.sh &&
+    build -q -n $(nproc) -a AARCH64 -b DEBUG -t GCC5 -p ArmVirtPkg/ArmVirtQemuKernel.dsc"
+    popd
+}
+
+clone
+build
