87b5facc75a0676927916036c9e7dd57dae7b595
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-01 14:08:38 -0700

    RME (Realm Management Extension) stack

--- Dockerfile
+++ Dockerfile
@@ -20,6 +20,12 @@ RUN apt update && apt install -y \
 e2fsprogs libarchive13t64 locales-all
 RUN apt update && apt install -y \
 libgnutls28-dev
+RUN apt update && apt install -y cmake python3-pyelftools
+RUN apt update && \
+apt install -y libbrotli-dev uuid-dev python-is-python3 acpica-tools
+RUN dpkg --add-architecture arm64 && \
+    apt update && apt install -y libfdt-dev:arm64
+
 RUN apt update && apt install -y ccache
 RUN apt update && apt install -y clang-tools
 RUN ln -s /usr/bin/intercept-build-* /usr/bin/intercept-build
---
+++ build_kvmtool.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+if [ -z "${DISABLE_CONTAINER_CHECK:-}" ]; then
+    ./container.sh ./build_kvmtool.sh
+    exit 0
+fi
+
+clone()
+{
+    rm -f kvmtool
+    url=https://gitlab.arm.com/linux-arm/kvmtool-cca
+    version=cca/v7
+    src=kvmtool_$(echo $version | tr '/' '_')
+    if [ ! -d $src ]; then
+        git clone $url --single-branch --branch $version --depth 1 $src
+    fi
+    ln -s $src kvmtool
+}
+
+build()
+{
+    pushd kvmtool
+    intercept-build --append \
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
+    sed -i compile_commands.json -e 's/"cc"/"aarch64-linux-gnu-gcc"/'
+    popd
+}
+
+output()
+{
+    mkdir -p out
+    rsync ./kvmtool/lkvm out/
+}
+
+clone
+build
+output
