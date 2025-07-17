8426aad81b062c530d1b2cd31afb7fab90631f9c
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-17 11:43:42 -0700

    build edk2 + tf-a + tf-rmm for sbsa

--- build_edk2.sh
+++ build_edk2.sh
@@ -13,7 +13,7 @@ clone()
     rm -f edk2
     url=https://github.com/tianocore/edk2.git
     version=edk2-stable202505
-    src=$version
+    src=$version-sbsa
     if [ ! -d $src ]; then
         git clone $url --single-branch --branch $version --depth 1 $src
         pushd $src
@@ -21,17 +21,50 @@ clone()
         popd
     fi
     ln -s $src edk2
+
+    # clone platforms, which contains sbsa platform
+    url=https://github.com/tianocore/edk2-platforms
+    # version should match edk2 revision
+    version=9562f2e64b2f817a7fd9455ce9dcd32d8500793d
+    src=edk2/edk2-platforms
+    if [ ! -d $src ]; then
+        git clone https://github.com/tianocore/edk2-platforms --depth 1 $src
+        pushd $src
+        git fetch origin $version --depth 1
+        git checkout $version
+        popd
+    fi
 }
 
 build()
 {
     pushd edk2
+    export PACKAGES_PATH=$(pwd):$(pwd)/edk2-platforms
+    # copy bin from TF-A
+    mkdir -p Platform/Qemu/Sbsa/
+    cp -f ../arm-trusted-firmware/build/qemu_sbsa/debug/*.bin Platform/Qemu/Sbsa/
+
     make -C BaseTools -j $(nproc)
     export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
+    # Set options to boot by default in EFI shell
     bash -c ". edksetup.sh &&
-    build -q -n $(nproc) -a AARCH64 -b DEBUG -t GCC5 -p ArmVirtPkg/ArmVirtQemuKernel.dsc"
+    build -q -n $(nproc) -a AARCH64 -b DEBUG -t GCC5 \
+    -D ENABLE_RME \
+    -p edk2-platforms/Platform/Qemu/SbsaQemu/SbsaQemu.dsc \
+    --pcd PcdUefiShellDefaultBootEnable=1 \
+    --pcd PcdShellDefaultDelay=0 \
+    --pcd PcdPlatformBootTimeOut=0"
+
+    truncate truncate -s 256M Build/SbsaQemuRme/DEBUG_GCC5/FV/SBSA_FLASH*.fd
     popd
+}
+
+output()
+{
+    mkdir -p out
+    rsync ./edk2/Build/SbsaQemuRme/DEBUG_GCC5/FV/SBSA_FLASH* out/
 }
 
 clone
 build
+output
--- gdbinit
+++ gdbinit
@@ -3,26 +3,26 @@ target remote :1234
 
 source ./gdb.py
 
-add-symbol-file ./arm-trusted-firmware/build/qemu/debug/bl1/bl1.elf
+add-symbol-file ./arm-trusted-firmware/build/qemu_sbsa/debug/bl1/bl1.elf
 b bl1_main
-add-symbol-file ./arm-trusted-firmware/build/qemu/debug/bl2/bl2.elf
+add-symbol-file ./arm-trusted-firmware/build/qemu_sbsa/debug/bl2/bl2.elf
 b bl2_main
-add-symbol-file ./arm-trusted-firmware/build/qemu/debug/bl31/bl31.elf
+add-symbol-file ./arm-trusted-firmware/build/qemu_sbsa/debug/bl31/bl31.elf
 b bl31_main
 
 # For rmm, we need to find loaded address (chosen by TF-A) + offset (build)
 # Check for RMM address loaded by TF-A:
 # $ ./run.sh qemu-system-aarch64 | grep 'Reserved RMM memory'
-# Reserved RMM memory [0x40100000, 0x418fffff] in Device tree
+# INFO:    Reserved RMM memory [0x10000000000, 0x10042ffffff] in Device tree
 # Check for offset in RMM image:
 # ./build_rmm.sh |& grep 'offset of the RMM core'
 # 0x20000
-add-symbol-file ./tf-rmm/build/Debug/rmm.elf 0x40120000
-#b rmm_main
+add-symbol-file ./tf-rmm/build/Debug/rmm.elf 0x10000020000
+b rmm_main
 
 # EDK2 directly prints add-symbol-file with expected offsets
 # only DxeCore is added here
-add-symbol-file edk2/Build/ArmVirtQemuKernel-AARCH64/DEBUG_GCC5/AARCH64/MdeModulePkg/Core/Dxe/DxeMain/DEBUG/DxeCore.dll 0xBF2BE000
+add-symbol-file ./edk2/Build/SbsaQemuRme/DEBUG_GCC5/AARCH64/MdeModulePkg/Core/Dxe/DxeMain/DEBUG/DxeCore.dll 0x1007F7CF000
 b DxeMain
 
 add-symbol-file ./linux/vmlinux
