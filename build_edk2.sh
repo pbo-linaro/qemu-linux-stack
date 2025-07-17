30adb59378cdf6e3658550cdd535843666a6ea49
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-17 11:43:42 -0700

    build edk2 + tf-a + tf-rmm for sbsa

--- build_edk2.sh
+++ build_edk2.sh
@@ -13,7 +13,7 @@ clone()
     rm -f edk2
     url=https://github.com/tianocore/edk2.git
     version=edk2-stable202505
-    src=$version
+    src=$version-sbsa
     if [ ! -d $src ]; then
         git clone $url --single-branch --branch $version --depth 1 $src
         pushd $src
@@ -21,20 +21,52 @@ clone()
         popd
     fi
     ln -s $src edk2
+
+    # clone platforms, which contains sbsa platform
+    url=https://github.com/tianocore/edk2-platforms
+    # version should match edk2 revision
+    version=9562f2e64b2f817a7fd9455ce9dcd32d8500793d
+    src=edk2/edk2-platforms
+    if [ ! -d $src ]; then
+        git clone https://github.com/tianocore/edk2-platforms --depth 1 $src
+        pushd $src
+        git fetch origin $version --depth 1
+        git checkout $version
+        popd
+    fi
 }
 
 build()
 {
     pushd edk2
+    export PACKAGES_PATH=$(pwd):$(pwd)/edk2-platforms
+    # copy bin from TF-A
+    mkdir -p Platform/Qemu/Sbsa/
+    cp -f ../arm-trusted-firmware/build/qemu_sbsa/debug/*.bin Platform/Qemu/Sbsa/
+
     make -C BaseTools -j $(nproc)
     export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
     # always build in debug to enable traces
+    # Set options to boot by default in EFI shell
     intercept-build --append \
     bash -c ". edksetup.sh &&
-    build -q -n $(nproc) -a AARCH64 -b DEBUG -t GCC5 -p ArmVirtPkg/ArmVirtQemuKernel.dsc"
+    build -q -n $(nproc) -a AARCH64 -b DEBUG -t GCC5 \
+    -D ENABLE_RME \
+    -p edk2-platforms/Platform/Qemu/SbsaQemu/SbsaQemu.dsc \
+    --pcd PcdUefiShellDefaultBootEnable=1 \
+    --pcd PcdShellDefaultDelay=0 \
+    --pcd PcdPlatformBootTimeOut=0"
     sed -i compile_commands.json -e 's/"cc"/"aarch64-linux-gnu-gcc"/'
+    truncate -s 256M Build/SbsaQemuRme/DEBUG_GCC5/FV/SBSA_FLASH*.fd
     popd
+}
+
+output()
+{
+    mkdir -p out
+    rsync ./edk2/Build/SbsaQemuRme/DEBUG_GCC5/FV/SBSA_FLASH* out/
 }
 
 clone
 build
+output
