2feb7293863cf13afbe4d331f86ade5d85ba399a
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-19 09:45:30 -0700

    device assignment: https://lists.trustedfirmware.org/archives/list/tf-rmm@lists.trustedfirmware.org/thread/WNGTY2TD7FQR4SW24EYKGV4IRGQZ67LJ/
    https://linaro.atlassian.net/wiki/spaces/QEMU/pages/30128767027/Device+Assignment+Enabled+RME+Stack+on+QEMU

--- run.sh
+++ run.sh
@@ -7,9 +7,7 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-set -x
-
-[ -v INIT ] || INIT=
+[ -v INIT ] || INIT=/host/host.sh
 
 mkdir -p out/EFI
 cp -f out/Image ./out/EFI/Image
@@ -17,6 +15,20 @@ cat > ./out/EFI/startup.nsh << EOF
 fs0:
 Image nokaslr root=/dev/vda rw init=/init -- $INIT
 EOF
+
+# Need to cd to spdm_responder_emu folder so it can access keys
+(cd out/spdm/ && ./spdm_responder_emu --trans PCI_DOE --slot_count 1)&
+spdm_pid=$!
+kill_spdm_emu()
+{
+    (kill $spdm_pid && wait $spdm_pid) || true
+}
+trap kill_spdm_emu EXIT
+
+# wait for spdm_emu
+sleep 1
+
+dd if=/dev/urandom of=out/disk bs=2M count=1
 
 "$@" \
 -nodefaults \
@@ -32,6 +44,11 @@ EOF
 -drive file=out/SBSA_FLASH1.fd,format=raw,if=pflash \
 -drive file=fat:rw:out/EFI,format=raw \
 -drive format=raw,file=./out/host.ext4,if=virtio \
--virtfs local,path=$(pwd)/,mount_tag=host,security_model=mapped,readonly=off
+-virtfs local,path=$(pwd)/,mount_tag=host,security_model=mapped,readonly=off \
+-device pcie-root-port,id=root_port,chassis=1,slot=0 \
+-drive id=disk,file=out/disk,if=none \
+-device ahci,id=ahci,spdm_port=2323,bus=root_port \
+-device ide-hd,drive=disk,bus=ahci.0 \
 
 rm -rf out/EFI
+rm -f out/disk
