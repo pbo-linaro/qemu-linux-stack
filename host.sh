2e07d62c3045c592b846bedb0809eced7dd81946
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-19 19:41:35 -0700

    update to da_alp12_v2: https://git.trustedfirmware.org/plugins/gitiles/TF-RMM/tf-rmm.git/+/refs/heads/topics/da_alp12_v2%5E%21/#F0

--- host.sh
+++ host.sh
@@ -7,8 +7,13 @@ set -x
 
 lspci -vvv
 
+disk_hash=$(md5sum < /dev/sda | cut -f 1 -d ' ')
+if [ -f /host/out/disk ]; then
+    diff <(echo $disk_hash) <(md5sum /host/out/disk | cut -f 1 -d ' ')
+fi
+
 # find device to connect
-sys_tsm_connect=$(find /sys | grep tsm/connect)
+sys_tsm_connect=$(find /sys | grep tsm/connect | tail -n 1)
 [ -f "$sys_tsm_connect" ]
 # /sys/devices/pci0000:00/0000:00:04.0/0000:01:00.0/tsm/connect -> 0000:01:00.0
 dev=$(basename $(realpath $(dirname $sys_tsm_connect)/..))
@@ -18,7 +23,7 @@ echo $dev
 echo $dev > /sys/bus/pci/devices/$dev/driver/unbind
 echo vfio-pci > /sys/bus/pci/devices/$dev/driver_override
 echo $dev > /sys/bus/pci/drivers_probe
-echo 1 > /sys/bus/pci/devices/$dev/tsm/connect
+echo tsm0 > /sys/bus/pci/devices/$dev/tsm/connect
 
 # prepare nested guest commands
 mkdir -p guest
@@ -30,11 +35,24 @@ set -x
 
 lspci -vvv
 
+diff <(echo $disk_hash) <(md5sum /dev/sda | cut -f 1 -d ' ')
+echo "unbind device"
 echo 0000:00:00.0 > /sys/bus/pci/devices/0000:00:00.0/driver/unbind
-echo 1 > /sys/bus/pci/devices/0000:00:00.0/tsm/connect
-echo 3 > /sys/bus/pci/devices/0000:00:00.0/tsm/connect
-echo 4 > /sys/bus/pci/devices/0000:00:00.0/tsm/connect
-#echo 0000:00:00.0 > /sys/bus/pci/drivers_probe
+echo "lock device"
+echo tsm0 > /sys/bus/pci/devices/0000:00:00.0/tsm/lock
+echo "set TDISP run state"
+echo 1 > /sys/bus/pci/devices/0000:00:00.0/tsm/accept
+
+echo "load driver"
+echo 0000:00:00.0 > /sys/bus/pci/drivers_probe
+
+[ -e /dev/sda ] || sleep 1
+[ -e /dev/sda ] || sleep 1
+[ -e /dev/sda ] || sleep 1
+[ -e /dev/sda ] || sleep 1
+[ -e /dev/sda ] || sleep 1
+[ -e /dev/sda ]
+diff <(echo $disk_hash) <(md5sum /dev/sda | cut -f 1 -d ' ')
 EOF
 chmod +x guest/da_connect.sh
 umount guest
@@ -42,34 +60,28 @@ rmdir guest
 
 INIT=${INIT:-/da_connect.sh}
 
-guest_efi=/tmp/guest_efi.img
-dd if=/dev/zero of=$guest_efi count=1 bs=256M
-mformat -i $guest_efi ::
-mcopy -i $guest_efi /host/out/Image ::
-cat > /tmp/startup.nsh << EOF
-fs0:
-Image nokaslr root=/dev/vda rw init=/init -- $INIT
-EOF
-mcopy -i $guest_efi /tmp/startup.nsh ::
-mdir -i $guest_efi
-
 cd /host
 ./out/lkvm run \
     --realm \
-    --in-kernel-smccc \
     -m 256 \
-    --no-pvtime \
-    --force-pci \
-    --firmware /host/out/KVMTOOL_EFI.fd \
+    --loglevel=debug \
+    --debug-mmio \
+    --debug-ioport \
+    --kernel /host/out/Image \
     --disk /host/out/guest.ext4 \
-    --disk $guest_efi \
-    --measurement-algo=sha256 \
-    --vfio-secure-pci $dev \
+    --iommufd-vdevice \
+    --vfio-pci $dev \
     --params "root=/dev/vda rw init=/init -- $INIT"
 
-rm -f $guest_efi /tmp/startup.nsh
-
-# rebind pci device to ahci
-echo $dev > /sys/bus/pci/devices/$dev/driver/unbind
-echo ahci > /sys/bus/pci/devices/$dev/driver_override
-echo $dev > /sys/bus/pci/drivers_probe
+## rebind pci device to ahci
+#echo $dev > /sys/bus/pci/devices/$dev/driver/unbind
+#echo ahci > /sys/bus/pci/devices/$dev/driver_override
+#echo $dev > /sys/bus/pci/drivers_probe
+#
+#[ -e /dev/sda ] || sleep 1
+#[ -e /dev/sda ] || sleep 1
+#[ -e /dev/sda ] || sleep 1
+#[ -e /dev/sda ] || sleep 1
+#[ -e /dev/sda ] || sleep 1
+#[ -e /dev/sda ]
+#diff <(echo $disk_hash) <(md5sum /dev/sda | cut -f 1 -d ' ')
