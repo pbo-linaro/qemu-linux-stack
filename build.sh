160d411b3a4d4ee5b77c919f80694867ff7f3852
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-09-29 14:09:24 -0700

    build: run check with podman and qemu-user-static

--- build.sh
+++ build.sh
@@ -4,6 +4,16 @@ set -euo pipefail
 
 rm -rf out
 
+if ! podman run -it --rm docker.io/debian:trixie echo; then
+    echo "error: podman must be installed on your machine"
+    exit 1
+fi
+
+if ! podman run -it --rm docker.io/arm64v8/debian:trixie echo; then
+    echo "error: qemu-user-static must be installed on your machine"
+    exit 1
+fi
+
 ./container.sh ccache -M 50GB
 
 ./build_kernel.sh
