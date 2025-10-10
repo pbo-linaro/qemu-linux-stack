3cf71c49c4fcd5cbcf9331cd30409c7376eeb33b
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-10-09 17:27:27 -0700

    linux: acs override patch to have different IOMMU groups for root port and sata device

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ mkdir -p out/EFI
 cp -f out/Image ./out/EFI/Image
 cat > ./out/EFI/startup.nsh << EOF
 fs0:
-Image nokaslr root=/dev/vda rw init=/init -- $INIT
+Image nokaslr root=/dev/vda rw init=/init pcie_acs_override=downstream,multifunction -- $INIT
 EOF
 
 # Need to cd to spdm_responder_emu folder so it can access keys
